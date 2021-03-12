04cc6e27208c6ca868eea903433a75ceb570f7e8
JackM1994 <36908600+JackM1994@users.noreply.github.com>
2021-03-12 16:09:34 +0000

    randomizing stocks with end of day btn

--- src/main.js
+++ src/main.js
@@ -1,9 +1,15 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+
 import App from './App.vue'
 import { routes } from './routes';
 import store from './store/store';
+
 Vue.use(VueRouter);
+
+Vue.filter('currency', (value) =>{
+	return '€' + value.toLocaleString();
+});
 
 const router = new VueRouter({
 	mode: 'history',
--- src/store/modules/stocks.js
+++ src/store/modules/stocks.js
@@ -9,6 +9,9 @@ const mutations = {
 		state.stocks = stocks;
 	},
 	'RND_STOCKS' (state){
+		state.stocks.forEach(stock =>{
+			stock.price = Math.round(stock.price * (1 + Math.random() - 0.5));
+		})
 
 	}
 };
@@ -20,7 +23,7 @@ const actions = {
 	initStocks: ({commit}) => {
 		commit('SET_STOCKS', stocks);
 	},
-	randommizeStocks: ({commit}) =>{
+	randomizeStocks: ({commit}) =>{
 		commit('RND_STOCKS');
 	}
 };
