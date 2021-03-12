6fc7a80a818287653f822bd4a0d20b6ca3c4cc4c
JackM1994 <36908600+JackM1994@users.noreply.github.com>
2021-03-12 16:17:15 +0000

    Added transitions between nav items

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,9 @@
     	<app-header></app-header>
     	<div class="row">
     		<div class="col-md-12">
-    			<router-view></router-view>
+                <transition name="slide" mode="out-in">
+                    <router-view></router-view>
+                </transition>
     		</div>
     	</div>
     </div>
@@ -25,4 +27,31 @@
 	body{
 		padding: 30px;
 	}
+    .slide-enter-active{
+        animation: slide-in 200ms ease-out forwards;
+    }
+    .slide-leave-active{
+        animation: slide-out 200ms ease-out forwards;
+    }
+    @keyframes slide-in{
+        from{
+            transform: translateY(-30px);
+            opacity: 0
+        }
+        to{
+            transform: translateY(0);
+            opacity: 1;
+        }
+    }
+
+    @keyframe slide-out{
+        from{
+            transform: translateY(0);
+            opacity: 1;
+        }
+        to {
+            transform: translateY(-30px);
+            opacity: 0;
+        }
+    }
 </style>
